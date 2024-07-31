ddbcd5379e43b55cc2290fb57f4db411893ca237
Uexcel2022 <excellenceudoka@gmail.com>
2024-07-31 15:18:09 +0100

    project repo created

---
+++ src/main/java/uexcel/com/reswebservices/ResWebservicesApplication.java
@@ -0,0 +1,13 @@
+package uexcel.com.reswebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uexcel/com/reswebservices/ResWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package uexcel.com.reswebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
